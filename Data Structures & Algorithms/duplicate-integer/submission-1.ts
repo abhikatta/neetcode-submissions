2153e2265e8243afb009628bcd7896807b49d1f6
Abhinay Katta <76813100+abhikatta@users.noreply.github.com>
2026-09-15 23:27:35 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const numsSet = new Set()
+        for (let i = 0; i < nums.length; i++) {
+            if (numsSet.has(nums[i])) {
+                return true
+            }
+            else {
+                numsSet.add(nums[i])
+            }
+        }
+        return false
+    }
+}
