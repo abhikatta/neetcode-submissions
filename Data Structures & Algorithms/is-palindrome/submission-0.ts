944af498e5f49460740f97158f7dc2cfe85e24b2
Abhinay Katta <76813100+abhikatta@users.noreply.github.com>
2026-09-17 01:10:10 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+
+        s = s.toLowerCase().replace(/[^A-Za-z0-9]/g, '')
+        const size = s.length
+
+       console.log(s)
+
+        for (let i = 0; i < size; i++) {
+            if (s[i] !== s[size - 1 - i]) {
+                return false
+            }
+        }
+        return true
+    }
+}
