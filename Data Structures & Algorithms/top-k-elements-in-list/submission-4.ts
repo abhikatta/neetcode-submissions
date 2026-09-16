07582b345ce855459d8dcb3e2840369c21006c8e
Abhinay Katta <76813100+abhikatta@users.noreply.github.com>
2026-09-16 18:34:56 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+
+        let first = 0,
+            last = nums.length - 1;
+        let freq = new Map<number, number>();
+        if (nums.length === 1) {
+            return nums
+        }
+        while (true) {
+            const newFirst = freq.get(nums[first])
+            freq.set(nums[first], (newFirst || 0) + 1);
+
+            const newLast = freq.get(nums[last])
+            freq.set(nums[last], (newLast || 0) + 1);
+
+            if (first + 1 === last || first === last) {
+                break
+            }
+            else {
+                first += 1
+                last -= 1
+            }
+        }
+
+        return [...freq.entries()]
+            .sort((a, b) => b[1] - a[1])
+            .slice(0, k)
+            .map(([key]) => key);
+
+    }
+}
