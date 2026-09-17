f54a9229f0f89755b7ed6d138507efbf08354c13
Abhinay Katta <76813100+abhikatta@users.noreply.github.com>
2026-09-17 17:07:24 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+
+        let start=0,
+        end=numbers.length-1
+        while(start<end){
+            if(numbers[start]+numbers[end]>target){
+                end-=1
+            }
+            else if (numbers[start]+numbers[end]<target){
+                start+=1
+            }
+            else{
+                return [start+1,end+1]
+            }
+            
+        }
+    }
+}
